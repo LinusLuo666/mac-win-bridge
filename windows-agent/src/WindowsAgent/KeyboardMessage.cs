7a060310c6a15aee21eed41f8d14fa9c42ce789f
BrianLuo <78671571+LinusLuo666@users.noreply.github.com>
2026-05-31 14:26:05 +0800

    Upload windows-agent/src/WindowsAgent/KeyboardMessage.cs

---
+++ windows-agent/src/WindowsAgent/KeyboardMessage.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WindowsAgent;
+
+public sealed record KeyboardMessage(
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("event")] string Event,
+    [property: JsonPropertyName("key")] string Key,
+    [property: JsonPropertyName("modifiers")] string[] Modifiers,
+    [property: JsonPropertyName("sequence")] long Sequence)
+{
+    public static KeyboardMessage? Parse(string line)
+    {
+        try
+        {
+            var message = JsonSerializer.Deserialize<KeyboardMessage>(line);
+            return message is { Type: "key" } ? message : null;
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+}
